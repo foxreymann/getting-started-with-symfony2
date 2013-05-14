ae2e1f1ccea03d72c9ca2ba1ef0841e1d9730a02
tomaszjureczko <tomasz.jureczko@gmail.com>
2013-05-14 12:27:53 +0100

    hello world

--- src/Sensio/Bundle/TrainingBundle/Controller/DefaultController.php
+++ src/Sensio/Bundle/TrainingBundle/Controller/DefaultController.php
@@ -5,7 +5,18 @@ namespace Sensio\Bundle\TrainingBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
+    /**
+     * @Route("/hello/{name}")
+     */
+    public function indexAction(Request $request)
+    {
+        $name = $request->attributes->get('name');
+        return new Response('Hello '.$name);
+    }
+
 }
