2b2cc76bb0c9b5310d6c29af792b9798d2b32af8
tomaszjureczko <tomasz.jureczko@gmail.com>
2013-05-14 14:57:33 +0100

    cookies example

--- src/Sensio/Bundle/TrainingBundle/Controller/DefaultController.php
+++ src/Sensio/Bundle/TrainingBundle/Controller/DefaultController.php
@@ -7,16 +7,37 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Cookie;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
+     * @Route("/hello/{name}", name="greet" )
+     */
+    public function helloAction($name)
+    {
+        $cookie = new Cookie('username', $name, new \DateTime('+30 days'));
+
+        $response = $this->redirect($this->generateUrl('hello'));
+        $response->headers->setCookie($cookie);
+
+        return $response;
+    }
+
+    /**
+     * @Route("/hello", name="hello" )
+     * @Template()
      */
     public function indexAction(Request $request)
     {
-        $name = $request->attributes->get('name');
-        return new Response('Hello '.$name);
-    }
+        if (!$name = $request->cookies->get('username')) {
+            throw $this->createNotFoundException('No username found'); 
+        }
+
+        return array('name' => $name);
+
+
+    }     
+
 
 }
