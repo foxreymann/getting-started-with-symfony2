de5ac21ba740d0f546d60756f67e0c4cc0738de4
tomaszjureczko <tomasz.jureczko@gmail.com>
2013-05-15 14:45:57 +0100

    todo show

--- src/Sensio/Bundle/TodoBundle/Controller/DefaultController.php
+++ src/Sensio/Bundle/TodoBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace Sensio\Bundle\TodoBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class DefaultController extends Controller
 {
@@ -37,4 +38,35 @@ class DefaultController extends Controller
 
         return array('count' => $count, 'tasks' => $tasks);
     }
+
+    /**
+     * @Route("/{id}/show", name="todo_show", requirements={"id" : "\d+"}) 
+     * @Template()
+     */
+    public function showAction($id) 
+    {
+        // Opening DB connection
+        if (!$conn = mysql_connect('127.0.0.1', 'root', '')) {
+            die('Unable to connect to MySQL : '. mysql_errno() .' '. mysql_error());
+        }
+
+        mysql_select_db('training_todo', $conn) or die('Unable to select database "training_todo"');
+        
+        // Retrive the task from db 
+        $result = mysql_query('SELECT * FROM todo WHERE id = '. $id);
+        $todo = mysql_fetch_assoc($result);
+
+        mysql_close($conn);
+
+        // forward to 404 if task not found
+        if(!$todo) {
+            throw new NotFoundHttpException(sprintf('Task #%d not found', $id));
+        }        
+
+    
+        // pass to twig
+        return array('todo' => $todo);
+
+    }
+
 }
