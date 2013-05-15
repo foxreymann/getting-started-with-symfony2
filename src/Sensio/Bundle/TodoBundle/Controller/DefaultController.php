98c1baa4fcf0436cf073cecef40629015f1c002c
tomaszjureczko <tomasz.jureczko@gmail.com>
2013-05-15 14:19:54 +0100

    todo refactoring listing

--- src/Sensio/Bundle/TodoBundle/Controller/DefaultController.php
+++ src/Sensio/Bundle/TodoBundle/Controller/DefaultController.php
@@ -15,8 +15,26 @@ class DefaultController extends Controller
     public function indexAction()
     {
         // Opening DB connection
-        
+        if (!$conn = mysql_connect('127.0.0.1', 'root', '')) {
+            die('Unable to connect to MySQL : '. mysql_errno() .' '. mysql_error());
+        }
 
-        return array();
+        mysql_select_db('training_todo', $conn) or die('Unable to select database "training_todo"');
+        
+        // Counting number of tasks in db
+        $result = mysql_query('SELECT COUNT(*) FROM todo', $conn);
+        $count  = current(mysql_fetch_row($result));
+
+        // Retrive all tasks in DB
+        $tasks = array();
+        $result = mysql_query('SELECT * FROM todo', $conn);
+
+        while ($todo = mysql_fetch_assoc($result)) {
+            $tasks[] = $todo;
+        }
+
+        mysql_close($conn);
+
+        return array('count' => $count, 'tasks' => $tasks);
     }
 }
